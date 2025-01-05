6bc3a145e6d84872bf2ad1dcf55d488a966164e1
Elyas Sharifi <elyassharifi2548@gmail.com>
2025-01-05 21:03:24 +0430

    Functions/sum

--- functions/sum/exercise/script.js
+++ functions/sum/exercise/script.js
@@ -8,22 +8,44 @@
   ES6 Syntax (Arrow function): const add = () => {}
 */
 
-function add(){
-  //Add function here
-}
+// // function add(a,b){
+// //   //Add function here
+// //   return a + b;
+// // }
+// console.log(add(2,2))
 
-function sub(){
-  //Subtract function here
-}
+// function sub(a, b){
+//   //Subtract function here
+//   return a - b;
+// }
+// console.log(sub(2,2));
 
-function div(){
-  //Divide function here
-}
+// function div(a, b){
+//   //Divide function here
+//   return a / b;
+// }
+// console.log(div(2,2));
 
-function mul(){
-  //Multiply function here
-}
+// function mul(a, b){
+//   //Multiply function here
+//   return a * b;
+// }
+// console.log(mul(2,2));
 
+
+//ES6 Arrow functions
+
+const add = (a,b) => a + b;
+console.log(add(2,2));
+
+const sub= (a, b) => a - b;
+console.log(sub(2, 2));
+
+const div = (a, b) => a / b;
+console.log(div(2, 2));
+
+const mul = (a, b) => a * b;
+console.log(mul(2, 2))
 console.log('hello from the SUM exercise')
 /* 
   TODO: create a function that console logs the result of any of the above operations.
